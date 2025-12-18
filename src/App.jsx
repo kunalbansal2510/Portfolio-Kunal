2181afe9ed2c8161b478019e25ba5bc384751ccf
Kunal <bansalkunal2510@gmail.com>
2025-12-18 18:01:59 +0530

    live

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Components/Navbar/navbar.jsx'
+import Home from './Components/Home/home.jsx' 
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
